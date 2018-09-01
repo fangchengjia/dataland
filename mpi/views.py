72ea72832768aaa161ea8ac300da432e1030b1d8
T-LAPTOP\count_t <fangcheng.jia@gmail.com>
2018-09-01 12:58:57 +1200

    add properties

--- mpi/views.py
+++ mpi/views.py
@@ -50,6 +50,34 @@ def incidents(request):
     })
 
 
+def alerts(request, zipcode):
+    incidents = []
+    for incident in models.Incident.objects.filter(zipcode=zipcode):
+        treatments = []
+        if incident.treatment != '':
+            for match_id in json.loads(incident.treatment):
+                record = models.Record.objects.get(id=match_id)
+                treatments.append({
+                    'name': record.name,
+                    'description': record.description,
+                    'url': record.url,
+                })
+            incidents.append({
+                'id': incident.id,
+                'name': incident.name,
+                'photoUrl': incident.photoUrl,
+                'zipcode': incident.zipcode,
+                'description': treatments[0]['description'],
+                'reportNotes': 'Myrtle rust has spread to many parts of New Zealand that have a suitable climate. If you believe you have seen myrtle rust: Do not touch the plant or the rust Call MPI\'s Exotic Pest and Disease Hotline on 0800 80 99 66. Note the location and if possible, take photos, including the type of plant the suspected rust is on. Do not attempt to touch or collect samples as this may increase the spread of this disease. If you accidently come in contact with the affected plant or the rust, make sure you bag your clothing and wash clothes, bags and shoes/boots when you get home.',
+                'distribution': 'This fungus is indigenous to Central and South America and the Caribbean. It also occurs in Florida. Myrtle rust was found in Hawaii in 2005, where it was initially found on ohia (Metrosideros polymorpha) a species closely related to pohutukawa and rata. It was later found on other hosts (all in the Myrtaceae family). It reached Australia in 2010, where it was initially detected on a property on the central coast of New South Wales. Since then it has spread across much of New South Wales, Queensland and Victoria. It has also been found in Tasmania and, now has spread to many parts of New Zealand that have a suitable climate.',
+                'symptoms': 'Myrtle rust attacks young, soft, actively growing leaves, shoot tips and young stems. Initial symptoms are powdery, bright yellow or orange-yellow pustules on leaves, tips and stems. The developing lesions may cause a deformation of the leaves and shoots, and twig dieback if the infection is severe. Symptoms also sometimes affect flowers and fruit. Infection of highly susceptible plants may result in plant death. Myrtle rust spores can be readily dispersed by wind or on clothing, equipment etc. Both modes of dispersal can transport spores very long distances.',
+                'timestamp': incident.timestamp
+            })
+    return http.JsonResponse({
+        'incidents': incidents
+    })
+
+
 def incident(request, incident_id):
     incident_detail = ''
     incident = models.Incident.objects.get(id=incident_id)
